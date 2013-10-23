44f267e7b7fcaebdc11dc64a4f57e5e1023cac6a
Van Dao <van.dao@kiss-concept.com>
2013-10-23 18:03:45 +0700

    change account connect database

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -17,8 +17,8 @@ return array(
             'db_english_media' => array(
                 'driver'         => 'Pdo',
                 'dsn'            => 'mysql:dbname=zf2tutorial;host=localhost',
-                'username' => 'root',
-                'password' => 'password',
+                'username' => 'developer',
+                'password' => 'kiss259',
                 'driver_options' => array(
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
                 ),
@@ -27,8 +27,8 @@ return array(
             'db_dictionary' => array(
                 'driver'         => 'Pdo',
                 'dsn'            => 'mysql:dbname=dictionary.traveldreams.vn;host=localhost',
-                'username' => 'root',
-                'password' => 'password',
+                'username' => 'developer',
+                'password' => 'kiss259',
                 'driver_options' => array(
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
                 ),
