b7feb6beae818adbad5e43844ebf2fc8ce8e619d
Van Dao <van.dao@kiss-concept.com>
2013-10-24 11:54:32 +0700

    change user login database

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -16,7 +16,7 @@ return array(
         'adapters' => array(
             'db_english_media' => array(
                 'driver'         => 'Pdo',
-                'dsn'            => 'mysql:dbname=zf2tutorial;host=localhost',
+                'dsn'            => 'mysql:dbname=em.traveldreams.vn;host=localhost',
                 'username' => 'developer',
                 'password' => 'kiss259',
                 'driver_options' => array(
