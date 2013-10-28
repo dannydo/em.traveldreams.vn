e3698f42f9b6e75b5456dfe8594738cc2522a180
Van Dao <van.dao@kiss-concept.com>
2013-10-28 16:29:31 +0700

    update database em.traveldreams.vn

--- database/em.traveldreams.vn_update.sql
+++ database/em.traveldreams.vn_update.sql
@@ -27,4 +27,27 @@ CREATE TABLE IF NOT EXISTS `Tags` (
   PRIMARY KEY (`TagId`),
   UNIQUE KEY `TagName` (`TagName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE  `Words` ADD  `IsToeic` BOOLEAN NOT NULL DEFAULT FALSE;
+
+CREATE TABLE IF NOT EXISTS `WordTags` (
+  `WordTagId` int(11) NOT NULL AUTO_INCREMENT,
+  `TagId` int(11) NOT NULL,
+  `WordId` int(11) NOT NULL,
+  PRIMARY KEY (`WordTagId`),
+  KEY `FK_WordTags_Tags_TagId_idx` (`TagId`),
+  KEY `FK_WordTags_Words_WordId_idx` (`WordId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `WordTags`
+ADD CONSTRAINT `FK_WordTags_Words_WordId_idx` FOREIGN KEY (`WordId`) REFERENCES `Words` (`WordId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `FK_WordTags_Tags_TagId_idx` FOREIGN KEY (`TagId`) REFERENCES `Tags` (`TagId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+INSERT INTO  `em.traveldreams.vn`.`Languages` (
+  `LanguageId` , `Name` , `Code` , `IsActive`
+)
+VALUES (NULL ,  'English',  'en',  '1'),
+       (NULL ,  'Vietnamese',  'vi',  '1');
+
+ALTER TABLE  `Sentences` ADD  `Order` INT( 2 ) NOT NULL
 /*======================================= End 28-10-2013 =======================================*/
