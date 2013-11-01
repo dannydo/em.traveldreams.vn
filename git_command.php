7bc02eca9cff768b5b333ca096ee21882fa6c7db
tiennguyen2012 <NGUYENVANTIEN2009@GMAIL.COM>
2013-11-01 15:44:56 +0700

    change git command line

--- git_command.php
+++ git_command.php
@@ -1,17 +1,6 @@
 <?php
-$path = "/home/web";
-$date = date('y-m-d');
-$github = "https://github.com/dannydo/em.traveldreams.vn.git";
+$path = "/home/web/respo/em.traveldreams.vn";
+//$date = date('y-m-d');
+//$github = "https://github.com/dannydo/em.traveldreams.vn.git";
 
-// check out
-$status = exec("cd ".$path.'/sites/staging.em.traveldreams.vn/'." \n git diff");
-if($status) {
-	// remove folder.
-	exec("rm -rf ". $path.'/respo/em.traveldreams.vn/'.$date);
-
-	exec("cd ".$path.'/respo/em.traveldreams.vn/'." \n git clone ".$github. " ".$path.'/respo/em.traveldreams.vn/'.$date);	
-
-	//add to stagign
-	exec("rm rf ".$path.'/sites/staging.em.traveldreams.vn');
-	exec("cd ".$path.'/sites/'." \n ln -s ".$path.'/respo/em.traveldreams.vn/'.$date." staging.em.traveldreams.vn");
-}
+exec("cd ".$path."/init \n git pull");
