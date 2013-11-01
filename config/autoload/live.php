44a5bb21f077dce7e042af865d4a1617999269b5
tiennguyen2012 <tien.nguyen@kiss-concept.com>
2013-11-01 16:41:07 +0700

    Change config of database

---
+++ config/autoload/live.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+
+return array(
+    'db' => array(
+        'adapters' => array(
+            'db_english_media' => array(
+                'driver'         => 'Pdo',
+                'dsn'            => 'mysql:dbname=em.traveldreams.vn;host=localhost',
+                'username' => 'developer',
+                'password' => 'kiss259',
+                'driver_options' => array(
+                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+                ),
+            ),
+
+            'db_dictionary' => array(
+                'driver'         => 'Pdo',
+                'dsn'            => 'mysql:dbname=dictionary.traveldreams.vn;host=localhost',
+                'username' => 'developer',
+                'password' => 'kiss259',
+                'driver_options' => array(
+                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+                ),
+            ),
+        ),
+    ),
+
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Db\Adapter\AdapterAbstractServiceFactory',
+        ),
+    ),
+);
